9bc48e0a547e5c56d4948e527b9e2f5119046c31
Nisarg Patel <marknisarg1997@gmail.com>
2023-06-07 17:21:45 -0400

    Add post comment, merge PR & close PR features

--- client/src/pages/ApiInterfaces.ts
+++ client/src/pages/ApiInterfaces.ts
@@ -1,9 +1,33 @@
 export interface RepositoryApiInterface {
-  contributors: string,
   description: string,
-  languages: string,
   name: string,
-  url: string
+  open_issues: number,
+  private: boolean,
+  html_url: string
+}
+
+export interface PullRequestsApiInterface {
+  base: {
+    ref: string
+  }
+  head: {
+    ref: string,
+    repo: {
+      name: string
+    }
+  },
+  html_url: string,
+  id: number,
+  number: number,
+  state: string,
+  title: string,
+  user: {
+    login: string
+  }
+}
+
+export interface PullRequestCommentsApiInterface {
+  body: string
 }
 
 export interface ApiErrorInterface {
--- server/proxy.py
+++ server/proxy.py
@@ -1,4 +1,5 @@
 import os
+from flask import jsonify
 import requests
 from dotenv import load_dotenv
 
@@ -14,8 +15,46 @@ class GithubProxy:
       'Accept': 'application/vnd.github+json'
     }
 
-  # Get data from Github using REST API.
-  def get_data(self, path):
-    url = f'{self.base_url}{path}'
-    response = requests.get(url, headers=self.headers)
+  # Get details of all repos.
+  def get_repos(self):
+    path = 'user/repos'
+    response = requests.get(f'{self.base_url}{path}', headers=self.headers)
     return response.json()
+
+  # Get all pull requests for a repository.
+  def get_pulls(self, path):
+    response = requests.get(f'{self.base_url}{path}', headers=self.headers)
+    return response.json()
+
+  # Post a comment to a pull request.
+  def post_comment(self, path, comment):
+    url = f'{self.base_url}{path}'
+    payload = {'body': comment}
+    response = requests.post(url, headers=self.headers, json=payload)
+
+    if response.status_code == 201:
+        return 'Comment posted successfully! Please check on GitHub!'
+    else:
+        return 'Failed to post comment!', 500
+
+  # Merge pull request.
+  def merge_pr(self, path):
+    url = f'{self.base_url}{path}'
+    print(url)
+    response = requests.put(url, headers=self.headers)
+
+    if response.status_code == 200:
+        return 'Pull request merged successfully! Please check on GitHub!'
+    else:
+        return 'Failed to merge pull request!', 500
+
+  # Post a comment to a pull request.
+  def close_pr(self, path):
+    url = f'{self.base_url}{path}'
+    payload = {'state': 'closed'}
+    response = requests.patch(url, headers=self.headers, json=payload)
+
+    if response.status_code == 200:
+        return 'Pull request closed successfully! Please check on GitHub!'
+    else:
+        return 'Failed to close pull request!', 500
